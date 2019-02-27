8cf078fe3ac26853c6619288812d415180055cdc
OtacilioN <otacilio.n97@gmail.com>
2019-02-27 09:42:10 -0300

    Add my first class in Java
    PS: Actually I really do not enjoy java

---
+++ ProgramacaoOrientadaObjetos/exercicios/firstClass/src/firstClass/Motor.java
@@ -0,0 +1,15 @@
+package firstClass;
+
+public class Motor {
+	
+	public void acelerar() {
+		System.out.print("acelerando!");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Motor meuMotor = new Motor();
+		meuMotor.acelerar();
+	}
+
+}
